00364d67a1e871b0d4ed005611c3ee430966478a
mironenko <mironenko@protei.ru>
2020-10-26 17:23:18 +0300

    lesson 2 (fix) - remove ./idea from git repository

--- src/lesson2/engine.ts
+++ src/lesson2/engine.ts
@@ -43,7 +43,6 @@ export const firstPrioritiesCalc = (stack: ParsedLineType): ParsedLineType =>
     } else {
       result.push(nextItem);
     }
-    console.log(result);
     return result;
   }, []);
 
